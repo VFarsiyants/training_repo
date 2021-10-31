b6e04656ca7e4c9066969a0578a746731494a289
Vladislav Farsiyants <farsiyanc@icloud.com>
2021-10-31 14:56:34 +0300

    Revert "Base python course tasks"
    This reverts commit 63e0a87c5c2b44bf899c4692760a78df405fa5de.

--- Python_Bases/Lesson4/task_4_5(4)/task_4_5(4).py
+++
@@ -1,6 +0,0 @@
-import sys
-from utils import get_currency
-
-print(f"на дату {get_currency(sys.argv[1])[1]} курс {sys.argv[1].upper()}: {get_currency(sys.argv[1])[0]}")
-
-# пример команды для запуска из консоли: python task_4_5(4).py NOK
